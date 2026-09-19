04f9ff4bdb20b1492521a2d4d657c0cbdd234db2
AmritaDeb <amritadebbarman@gmail.com>
2026-09-19 17:59:39 +0530

    First test

---
+++ tests/myFirstTest.spec.ts
@@ -0,0 +1,13 @@
+import {test, expect} from '@playwright/test';
+
+test('My First Test', async({page}) => {
+
+    await page.goto('https://www.google.com/');
+    await page.locator('textarea[name="q"]').fill('Playwright');
+    // await page.keyboard.press('Enter');
+    await page.waitForTimeout(2000);
+    // const results = await page.locator('h3').allTextContents;
+    // expect(results.length).toBeGreaterThan(0);
+    const results = await page.locator('//span[contains(text(),"playwright")]').count();
+    expect (results).toBeGreaterThan(0);
+});
